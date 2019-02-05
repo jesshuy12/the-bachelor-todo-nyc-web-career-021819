2b045c41f7b5302d99c841f638046c949378f94a
Jessy Hu <jesshuy12@gmail.com>
2019-02-05 04:58:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,45 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |key, value|               #iterates over the hash "data"
+    if key == season                      #determine if the keys within the data hash is equal to                                          season arguement
+      value.each do |info|               #iterates over the values in the hash "data"
+    if info["status"] == "Winner"        #checks to see if the status is equal to winner
+      return info["name"].split[0]       #if status is winner, returns that full name, but split                                    and only returning the value in index 0
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, value|
+    value.each do |info|
+      if info["occupation"] == occupation
+        return info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0                       #create a counter starting at 0 
+  data.each do |key, value|
+    value.each do |info|
+      if info["hometown"] == hometown   #determining if both value and argmement match
+        counter += 1                    #adding 1 to the counter everytime if "hometown" matches                                  hometown arguement
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |key, value|
+    value.each do |info|
+      if info["hometown"] == hometown
+        return info["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
